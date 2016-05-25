1005df7a9d378523a24750e06b08395795e46aeb
Brian OMeara <omeara.brian@gmail.com>
2016-05-25 16:48:46 -0400

    now with namespace and more help on using API

--- R/main.R
+++ R/main.R
@@ -1,18 +1,21 @@
-hello <- function (myname = "")
-{
-    if (myname == "") {
-        stop("Tell me your name!")
-    }
-    list(message = paste("hello", myname, "! This is", R.Version()$version.string))
-}
-
+#' Get citations for all the trees currently in the cache
+#' @return A message with the list of publications
+#' @export
 GetAllCitations <- function() {
   data(datelife::opentree_chronograms)
   publications <- paste('<ul>', paste('<li>',names(datelife.cache$trees), '</li>', sep="", collapse=""), '</ul>', sep="")
   return(list(message=publications))
 }
 
-run <- function(input, format, partialfield="liberal", usetnrsfield="no", approximatematchfield="no") {
+#' The core function to interact with DateLife via the web or API
+#' @param input The string to pass to R
+#' @param format The return format desired
+#' @param partialfield To be liberal or conservative with returning trees that only match some of the taxa
+#' @param usetnrsfield Yes or no to use taxonomic name resolution
+#' @param approximatematchfield Yes or no to use approximate name matching
+#' @return A message in the appropriate format
+#' @export
+run <- function(input, format="mrca", partialfield="liberal", usetnrsfield="no", approximatematchfield="no") {
   #taxa <- c("Rhea americana", "Pterocnemia pennata", "Struthio camelus")
   #results.list <- lapply(datelife.cache$trees,GetSubsetArrayDispatch, taxa=taxa, phy=NULL)
 #  filtered.results <- datelife::ProcessResultsList(results.list, taxa, TRUE)
--- README.md
+++ README.md
@@ -8,7 +8,38 @@ library(opencpu)
 opencpu$browse("library/datelifeweb/www")
 ```
 
+To test once it's running, you can use the GUI in the web browser that opens and play with it. If you want to use the API:
 
+```
+curl http://localhost:5802/ocpu/library/datelifeweb/R/run -d "input='Rhinoceros%20unicornis%2CEquus%20caballus%2CMus%20musculus'&format='mrca' "
+```
+
+[changing the URL as appropriate]
+
+This will give you back something like
+```
+/ocpu/tmp/x0ea8fe25ca/R/.val
+/ocpu/tmp/x0ea8fe25ca/stdout
+/ocpu/tmp/x0ea8fe25ca/source
+/ocpu/tmp/x0ea8fe25ca/console
+/ocpu/tmp/x0ea8fe25ca/info
+/ocpu/tmp/x0ea8fe25ca/files/DESCRIPTION
+```
+
+You can then get values:
+
+```
+curl http://localhost:5802/ocpu/tmp/x0ea8fe25ca/R/.val/print
+```
+
+returns
+```
+$message
+Bininda-Emonds, Olaf R. P., Marcel Cardillo, Kate E. Jones, Ross D. E. MacPhee, Robin M. D. Beck, Richard Grenyer, Samantha A. Price, Rutger A. Vos, John L. Gittleman, Andy Purvis. 2007. The delayed rise of present-day mammals. Nature 446 (7135): 507-512
+101.3
+Nyakatura, Katrin, Olaf RP Bininda-Emonds. 2012. Updating the evolutionary history of Carnivora (Mammalia): a new species-level supertree complete with divergence time estimates. BMC Biology 10 (1): 12
+105.7
+```
 
 
 Old install code:
