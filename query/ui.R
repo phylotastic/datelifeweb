c37f2d5338c52c5c16d1e194fb7535ab5c2598d1
Brian O'Meara <omeara.brian@gmail.com>
2016-09-04 08:54:23 -0400

    removed styling from query

--- query/ui.R
+++ query/ui.R
@@ -1,7 +1,6 @@
 library(shiny)
 
 shinyUI(fluidPage(
-   theme = "default.css",
 textInput("taxa", "Taxa (comma delimited)", "Rhea americana, Pterocnemia pennata, Struthio camelus", width="100%"),
  checkboxInput('partial', 'Include studies with a subset of desired taxa, perhaps resulting in underestimate of ages', TRUE),
  br(),
