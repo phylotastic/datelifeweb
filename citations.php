96361240e504810d7f000360d40f87077cb0e251
Brian O'Meara <omeara.brian@gmail.com>
2016-09-05 21:21:54 -0400

    update everything to html extension

--- citations.php
+++
@@ -1,8 +0,0 @@
-<html>
-<?php include("pagestart.html"); ?>
-<h2>API</h2>
-<br />
-<p>This page describes the current API for DateLife. Note that it is still alpha: it may break or change.</p>
-
-
-<?php include("pageend.html"); ?>
