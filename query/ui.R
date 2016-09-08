9a5f4c734cd2f94e8672231579ff0db887b74ad4
Brian OMeara <omeara.brian@gmail.com>
2016-09-08 12:44:08 -0400

    better spacing

--- query/ui.R
+++ query/ui.R
@@ -1,7 +1,7 @@
 library(shiny)
 
 shinyUI(fluidPage(
-textInput("taxa", "Taxa (comma delimited)", "Rhea americana, Pterocnemia pennata, Struthio camelus", width="100%"),
+textInput("taxa", "Taxa (comma delimited, spaces are ok) or Tree (Newick format, make sure to end with a semicolon)", "Rhea americana, Pterocnemia pennata, Struthio camelus", width="100%"),
  checkboxInput('partial', 'Include studies with a subset of desired taxa, perhaps resulting in underestimate of ages', TRUE),
  br(),
  h2("Table of ages and sources"),
