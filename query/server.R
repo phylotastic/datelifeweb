8b0a3b4f5467d984921d77b2a41b9865820e1417
Brian O'Meara <omeara.brian@gmail.com>
2017-03-14 10:14:12 -0400

    trying to make flexible output for multiple pages using trycatch to deal with missing elements on some pages

--- query/server.R
+++ query/server.R
@@ -39,14 +39,14 @@ observe({
    temp.tree
    })
 
- output$age <- renderTable(SummarizeResults(get.filtered.results(), output.format="data.frame"))
- output$medianPlot <- renderPlot(geoscalePhylo(get.consensus.tree(), cex.tip=2, cex.ts=2, cex.age=2, units=c("Era", "Period"), boxes="Period"))
- output$citations <- dump.citations()
+ tryCatch({output$age <- renderTable(SummarizeResults(get.filtered.results(), output.format="data.frame"))})
+ tryCatch({output$medianPlot <- renderPlot(geoscalePhylo(get.consensus.tree(), cex.tip=2, cex.ts=2, cex.age=2, units=c("Era", "Period"), boxes="Period"))})
+ tryCatch({output$citations <- dump.citations()})
 
- output$downloadCSV <- downloadHandler(
+ tryCatch({output$downloadCSV <- downloadHandler(
    filename = "DatelifeTable.csv",
    content = function(file) {
      write.csv(SummarizeResults(get.filtered.results(), output.format="data.frame"), file)
    }
- )
+ )})
 })
