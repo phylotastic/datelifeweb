e559ac91d2faaf440083abb4ba885cae61b455ce
Brian O'Meara <omeara.brian@gmail.com>
2017-05-06 00:18:49 -0500

    updating for to allow downloads

--- contributors/server.R
+++ contributors/server.R
@@ -14,4 +14,22 @@ data(treebasecache)
  output$authors<- renderDataTable(author.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")) , order = list(1, 'desc')))
  output$curators<- renderDataTable(curator.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")), order = list(1, 'desc')))
  output$tbauthors<- renderDataTable(tb.author.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")) , order = list(1, 'desc')))
+ output$downloadauthorsCSV <- downloadHandler(
+   filename = "authors.csv",
+   content = function(file) {
+     write.csv(author.pretty, file=file)
+   }
+ )
+ output$downloadcuratorsCSV <- downloadHandler(
+   filename = "curators.csv",
+   content = function(file) {
+     write.csv(curator.pretty, file=file)
+   }
+ )
+ output$downloadtbauthorsCSV <- downloadHandler(
+   filename = "treebase_authors.csv",
+   content = function(file) {
+     write.csv(tb.author.pretty, file=file)
+   }
+ )
 })
--- contributors/ui.R
+++ contributors/ui.R
@@ -2,11 +2,14 @@ library(shiny)
 
 shinyUI(fluidPage(
  h2("Contributors"),
- p("Work on reusing trees requires people to do the work of doing the research and releasing their results (authors) and curating them for later reuse (curators). The tables below are the authors and curators of studies used in OpenTree's synthetic tree (and so doesn't reflect deposited trees that are not marked for synthesis or which have been uploaded since the last synthetic tree was computed) and the authors of trees in TreeBase."),
+ p("Work on reusing trees requires people to do the work of doing the research and releasing their results (authors) and curating them for later reuse (curators). The tables below are the authors and curators of studies used in OpenTree's synthetic tree (and so doesn't reflect deposited trees that are not marked for synthesis or which have been uploaded since the last synthetic tree was computed) and the authors of trees in TreeBase. You can search and sort in the tables."),
  h3("OpenTree Authors"),
+ downloadLink('downloadauthorsCSV', 'OpenTree author table in csv format'),
  dataTableOutput("authors"),
  h3("OpenTree Curators"),
+ downloadLink('downloadcuratorsCSV', 'OpenTree curator table in csv format'),
  dataTableOutput("curators"),
  h3("TreeBase Authors"),
+ downloadLink('downloadtbauthorsCSV', 'TreeBase author table in csv format'),
  dataTableOutput("tbauthors")
 ))
