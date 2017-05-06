39450c31c7f2d51f8d36ad878e29a7eecf4d2d6a
Brian O'Meara <omeara.brian@gmail.com>
2017-05-06 00:14:18 -0500

    treebase authors, too

--- contributors/server.R
+++ contributors/server.R
@@ -1,11 +1,17 @@
+datelife.exists <- suppressWarnings(library("datelife", quietly = TRUE, logical.return=TRUE))
+if(!datelife.exists) {
+  devtools::install_github("phylotastic/datelife")
+  library(datelife)
+}
 library(shiny)
-library(datelife)
 library(strap)
 library(ape)
 
 shinyServer(function(input, output, session) {
 
 data(contributorcache)
- output$authors<- renderTable(author.pretty, output.format="data.frame")
- output$curators<- renderTable(curator.pretty, output.format="data.frame")
+data(treebasecache)
+ output$authors<- renderDataTable(author.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")) , order = list(1, 'desc')))
+ output$curators<- renderDataTable(curator.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")), order = list(1, 'desc')))
+ output$tbauthors<- renderDataTable(tb.author.pretty, options = list(lengthMenu = list(c(25, 50, 100,-1), c("25", "50", "100", "All")) , order = list(1, 'desc')))
 })
--- contributors/ui.R
+++ contributors/ui.R
@@ -2,9 +2,11 @@ library(shiny)
 
 shinyUI(fluidPage(
  h2("Contributors"),
- p("Work on reusing trees requires people to do the work of doing the research and releasing their results (authors) and curating them for later reuse (curators). The tables below are the authors and curators of studies used in OpenTree's synthetic tree."),
- h3("Authors"),
- tableOutput("authors"),
- h3("Curators"),
- tableOutput("curators")
+ p("Work on reusing trees requires people to do the work of doing the research and releasing their results (authors) and curating them for later reuse (curators). The tables below are the authors and curators of studies used in OpenTree's synthetic tree (and so doesn't reflect deposited trees that are not marked for synthesis or which have been uploaded since the last synthetic tree was computed) and the authors of trees in TreeBase."),
+ h3("OpenTree Authors"),
+ dataTableOutput("authors"),
+ h3("OpenTree Curators"),
+ dataTableOutput("curators"),
+ h3("TreeBase Authors"),
+ dataTableOutput("tbauthors")
 ))
